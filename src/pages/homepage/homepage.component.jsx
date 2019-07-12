ba583e149276fe59ac218054013e60f247c44d77
Prince Eddy <princeeddy@Princes-MacBook-Air.local>
2019-07-12 18:31:17 +0800

    update our menu items to navigate withRouter

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.components'
+import './homepage.styles.scss'
+const HomePage = ()=>(
+    <div className='home-page'>
+        <Directory />
+    </div>
+);
+export default HomePage;
